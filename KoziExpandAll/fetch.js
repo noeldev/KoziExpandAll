2e0aaf9787e68735b81eb6d5a05ca17fb00fe56a
Noël Danjou <noeldev@users.noreply.github.com>
2025-03-29 01:32:01 +0100

    fix: minor updates to doc and fetch.js

--- KoziExpandAll/fetch.js
+++ KoziExpandAll/fetch.js
@@ -1,4 +1,3 @@
 const script = document.createElement("script");
 script.src = "https://cdn.jsdelivr.net/gh/noeldev/KoziExpandAll/KoziExpandAll/KoziExpandAll.js";
-script.onload = () => console.log("Script loaded from jsDelivr!");
 document.body.appendChild(script);
